3ca5ff26dc3b7bb2593030e1035424503fdfc70d
SarfarazAliToori <alifarazzain@gmail.com>
2021-10-17 00:46:51 +0500

    add Log Info

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_message.setOnClickListener(View.OnClickListener {
+            Log.i("Main Activity", "Hello World from Log cat")
+            Toast.makeText(this, "Hello World from Toast", Toast.LENGTH_SHORT).show()
+
+        })
     }
 }
